0c146502fc828358354fc534a4ca123486f517c6
Martynezys <lenlein@proton.me>
2023-01-09 18:15:58 +0100

    Add project files.

---
+++ 10.03/Book.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Book
+    {
+        private string title;
+        public string Title 
+        {
+          get { return title; }
+          set { title = value; }
+        }
+        private int pages;
+        public int Pages
+        {
+            get { return pages; }
+            set
+            {
+
+                if (pages < 0)
+                {
+                    pages = 1;
+                }
+                else
+                {
+                    pages = value;
+                }
+            }
+        }
+        private string category;
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        private string author;
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        private int releaseDate;
+        public int ReleaseDate
+        {
+            get { return releaseDate; }
+            set  
+            {
+                if (releaseDate < 1450 && releaseDate > 2021)
+                {
+                    releaseDate = -1;
+                }
+                else
+                {
+                    releaseDate = value;
+                }
+            }
+        }
+
+
+
+
+        public void OlkoBolko()
+        {
+            Console.WriteLine(Title);
+            Console.WriteLine(Pages);
+            Console.WriteLine(Category);
+            Console.WriteLine(Author);
+            Console.WriteLine(ReleaseDate);
+
+        }
+
+
+    }
+}
---
+++ 10.03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR=new Book();
+            
+            
+            LOTR.Title= "Lord of The Rings";
+            LOTR.Pages= 1028;
+            LOTR.Category= "Fantazy";
+            LOTR.Author= "Oliver Krska";
+            LOTR.ReleaseDate= 1985;
+            LOTR.OlkoBolko();
+            Console.ReadLine();
+        }
+    }
+}
